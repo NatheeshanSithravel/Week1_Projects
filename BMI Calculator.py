6246df094a0269e3765d9b351de6cb9cfac668bc
Natheeshan Sithravel <natheeshan.20244025@iit.ac.lk>
2024-10-07 15:40:42 +0530

    Add files via upload

---
+++ BMI Calculator.py
@@ -0,0 +1,17 @@
+#Author Natheeshan
+#Date Oct-07-2024
+#BMI Calculator
+
+"""
+Get weight in KiloGrams
+Get height in Meter
+Calculate BMI and Show Result
+
+"""
+
+w = int(input("Enter Your Weight in KG"))
+h = float(input("Enter Your Height in METER"))
+
+BMI = w/h**2
+
+print(BMI)
